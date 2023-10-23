5bae03d7a2ceb3ee9ac80074104c399439b06cda
FtKuBo <semmarmehdi558@gmail.com>
2023-10-23 16:39:26 -0400

    webcv

--- js/phone.js
+++ js/phone.js
@@ -1,2 +1,37 @@
-let openPopup= document.getElementById("tel");
-console.log(openPopup);
+console.log("helloworld")
+
+const openPppup = document.querySelectorAll('[popup-open]');
+const closePppup = document.querySelectorAll('[data-dismiss-popup]');
+const overlay = document.getElementById('overlay');
+
+
+overlay.addEventListener('click', () =>{
+    const popup=document.getElementById('ppup');
+    closePopup(popup);
+})
+
+openPppup.forEach(button => {
+    button.addEventListener('click', () => {
+        const popup=document.getElementById('ppup');
+        openPopup(popup);
+    })
+})
+
+closePppup.forEach(button => {
+    button.addEventListener('click', () => {
+        const popup=button.closest('.popup');
+        closePopup(popup);
+    })
+})
+
+function openPopup(popup) {
+    if (popup == null) return; 
+    popup.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closePopup(popup) {
+    if (popup == null) return; 
+    popup.classList.remove('active');
+    overlay.classList.remove('active');
+}
